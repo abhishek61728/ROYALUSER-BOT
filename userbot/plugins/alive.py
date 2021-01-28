22c28b1c528d6b9efaa41562f537bf3bab73f124
incredible-Boi <76954119+incredible-Boi@users.noreply.github.com>
2021-01-28 12:54:32 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -14,7 +14,7 @@ if ludosudo:
 else:
     sudou = "False"
 
-kraken = bot.uid
+ABHISHEK = bot.uid
 
 PM_IMG = "https://telegra.ph/file/b3818868fea51e007bae6.jpg"
 pm_caption = "__**🔥🔥𝚁𝚘𝚢𝚊𝚕𝚄𝚜𝚎𝚛-𝙱𝚘𝚝🔥🔥**__\n\n"
