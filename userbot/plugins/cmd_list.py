3bd169256c1154aa0db338f58a75fe21f267efb2
ROYAL-ABHISHEK <73883779+abhishek61728@users.noreply.github.com>
2021-01-29 09:20:38 +0530

    Update cmd_list.py

--- userbot/plugins/cmd_list.py
+++ userbot/plugins/cmd_list.py
@@ -2,14 +2,14 @@ import asyncio
 import io
 
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot import bot as hellbot
+from userbot import bot as royalbot
 from userbot import ALIVE_NAME
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "HellBot"
-hell_logo = "./KRAKEN/hellbot_logo.jpg"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "RoyalBot"
+royal_logo = ".https://telegra.ph/file/5a38108b37dfe0dc2040c.jpg"
 
-@hellbot.on(admin_cmd(pattern=r"cmds"))
-@hellbot.on(sudo_cmd(pattern=r"cmds", allow_sudo=True))
+@royalbot.on(admin_cmd(pattern=r"cmds"))
+@royalbot.on(sudo_cmd(pattern=r"cmds", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
@@ -17,7 +17,7 @@ async def install(event):
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     cmd = "ls userbot/plugins"
-    thumb = hell_logo
+    thumb = royal_logo
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -25,11 +25,11 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"List of Plugins in HellBot :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/hellbot_official_chat for help."
+    OUTPUT = f"List of Plugins in RoyalBot :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/RoyalUserBot_Official for help."
     if len(OUTPUT) > 69:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "cmd_list.text"
-            hell_file = await bot.send_file(
+            royal_file = await bot.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
@@ -37,5 +37,5 @@ async def install(event):
                 thumb=thumb,
                 reply_to=reply_to_id,
             )
-            await edit_or_reply(hell_file, f"Output Too Large. This is the file for the list of plugins in hellbot.\n\n**BY :-** {DEFAULTUSER}")
+            await edit_or_reply(royal_file, f"Output Too Large. This is the file for the list of plugins in royalbot.\n\n**BY :-** {DEFAULTUSER}")
             await event.delete()
